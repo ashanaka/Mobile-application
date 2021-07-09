97b9410593376d6f03b257bc48609f743fcb6e71
Ashan Shanaka <44833876+ashanaka@users.noreply.github.com>
2021-07-09 14:24:29 +0530

    test

--- src/navigators/StackNavigator.js
+++ src/navigators/StackNavigator.js
@@ -16,9 +16,9 @@ const Stack = createStackNavigator();
 const MainStackNavigator = () => {
   return (
     <Stack.Navigator headerMode={'none'}>
-      {/*<Stack.Screen name={'SplashScreen'} component={SplashScreen} />*/}
-      {/*<Stack.Screen name={'SignInScreen'} component={SignInScreen} />*/}
-      {/*<Stack.Screen name={'SignUpScreen'} component={SignUpScreen} />*/}
+      <Stack.Screen name={'SplashScreen'} component={SplashScreen} />
+      <Stack.Screen name={'SignInScreen'} component={SignInScreen} />
+      <Stack.Screen name={'SignUpScreen'} component={SignUpScreen} />
       <Stack.Screen name={'CommonHome'} component={HomeScreen} />
       <Stack.Screen name={'ConsumerHome'} component={ConsumerDrawerNavigator} />
       <Stack.Screen name={'GrowerHome'} component={GrowerDrawerNavigator} />
